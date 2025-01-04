3a72488f87db4541042fb1e6945f915d7125fb07
Mac <mac@192.168.1.5>
2025-01-04 13:32:30 +0200

    code

--- src/Pages/Protfolio/Protfolio.jsx
+++ src/Pages/Protfolio/Protfolio.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
-import NavBar from '../../Components/Nav/Nav'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+
 export default function Protfolio() {
   return <>
-  <div>prooooo</div>
-  </>
+<Container  className="justify-content-md-center">
+      <Row>
+          <Col  >
+        <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+    </Card>
+          </Col>
+          <Col >
+        <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+    </Card>
+          </Col>
+          <Col>
+        <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+    </Card>
+          </Col>
+      </Row>
+    </Container>
+
+ 
+     </>
 }
